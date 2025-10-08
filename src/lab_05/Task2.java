c619a1a7502368f32cf61d5b1e10811b4162441b
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-10-08 10:10:21 -0400

    initial commit

--- src/lab_05/Task2.java
+++ src/lab_05/Task2.java
@@ -8,6 +8,17 @@ package lab_05;
  *
  * @author 6298805
  */
-public class Task2 {
-    
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.stage.Stage;
+
+public class Task2 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        
+    }
 }
