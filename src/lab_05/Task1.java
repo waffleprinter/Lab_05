071afa7ca2e15cc4a1665b5893968f9bcdd48916
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-10-08 09:48:52 -0400

    Initial commit

---
+++ src/lab_05/Task1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab_05;
+
+/**
+ *
+ * @author 6298805
+ */
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Task1 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        
+    }
+}
---
+++ src/lab_05/Task2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab_05;
+
+/**
+ *
+ * @author 6298805
+ */
+public class Task2 {
+    
+}
