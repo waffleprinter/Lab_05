c0d01fd91d08d0d353f694bf53cc0f23bec670c1
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-10-08 11:28:30 -0400

    made food items into Food objects instead of String

--- src/lab_05/Task2.java
+++ src/lab_05/Task2.java
@@ -8,6 +8,7 @@ package lab_05;
  *
  * @author 6298805
  */
+import java.util.Objects;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.geometry.Insets;
@@ -29,8 +30,15 @@ public class Task2 extends Application {
         // Beverages
         Label beveragesLabel = new Label("Beverages");
         
-        ComboBox<String> beverages = new ComboBox<>();
-        beverages.getItems().addAll("Coffee", "Tea", "Soft Drink", "Water", "Milk", "Juice");
+        ComboBox<Food> beverages = new ComboBox<>();
+        beverages.getItems().addAll(
+                new Food("Coffee", 2.50),
+                new Food("Tea", 2.00),
+                new Food("Soft Drink", 1.75),
+                new Food("Water", 2.95),
+                new Food("Milk", 1.5),
+                new Food("Juice", 2.5)
+        );
         
         VBox beveragesVBox = new VBox(beveragesLabel, beverages);
         beveragesVBox.setAlignment(Pos.CENTER);
@@ -38,8 +46,14 @@ public class Task2 extends Application {
         // Appetizers
         Label appetizersLabel = new Label("Appetizers");
         
-        ComboBox<String> appetizers = new ComboBox<>();
-        appetizers.getItems().addAll("Soup", "Salad", "Spring Rolls", "Garlic Bread", "Chips and Salsa");
+        ComboBox<Food> appetizers = new ComboBox<>();
+        appetizers.getItems().addAll(
+                new Food("Soup", 2.50),
+                new Food("Salad", 2.00),
+                new Food("Spring Rolls", 1.75),
+                new Food("Garlic Bread", 2.95),
+                new Food("Chips and Salsa", 1.5)
+        );
         
         VBox appetizersVBox = new VBox(appetizersLabel, appetizers);
         appetizersVBox.setAlignment(Pos.CENTER);
@@ -47,10 +61,16 @@ public class Task2 extends Application {
         // Main course
         Label mainCourseLabel = new Label("Main Course");
         
-        ComboBox<String> mainCourse = new ComboBox<>();
+        ComboBox<Food> mainCourse = new ComboBox<>();
         mainCourse.getItems().addAll(
-               "Steak", "Grilled Chicken", "Chicken Alfredo", 
-                "Turkey Club", "Shrimp Scampi", "Pasta", "Fish and Chips");
+                new Food("Steak", 15.00),
+                new Food("Grilled Chicken", 13.50),
+                new Food("Chicken Alfredo", 13.95),
+                new Food("Turkey Club", 11.90),
+                new Food("Shrimp Scampi", 18.99),
+                new Food("Pasta", 11.75),
+                new Food("Fish and Chips", 12.25)
+        );
         
         VBox mainCourseVBox = new VBox(mainCourseLabel, mainCourse);
         mainCourseVBox.setAlignment(Pos.CENTER);
@@ -58,8 +78,14 @@ public class Task2 extends Application {
         // Dessert
         Label dessertLabel = new Label("Dessert");
         
-        ComboBox<String> dessert = new ComboBox<>();
-        dessert.getItems().addAll("Coffee", "Tea", "Soft Drink", "Water", "Milk", "Juice");
+        ComboBox<Food> dessert = new ComboBox<>();
+        dessert.getItems().addAll(
+                new Food("Apple Pie", 5.95),
+                new Food("Carrot Cake", 4.50),
+                new Food("Mud Pie", 4.75),
+                new Food("Pudding", 3.25),
+                new Food("Apple Crisp", 5.98)
+        );
         
         VBox dessertVBox = new VBox(dessertLabel, dessert);
         dessertVBox.setAlignment(Pos.CENTER);
@@ -74,5 +100,20 @@ public class Task2 extends Application {
         stage.setScene(scene);
         stage.setTitle("Menu");
         stage.show();
+    }
+    
+    private class Food {
+        private String name;
+        private double price;
+        
+        public Food(String name, double price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
     }
 }
