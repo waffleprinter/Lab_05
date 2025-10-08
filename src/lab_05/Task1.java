34c2d63ebe9558a475ee6c6c7710031ded559b76
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-10-08 10:10:14 -0400

    added style selector

--- src/lab_05/Task1.java
+++ src/lab_05/Task1.java
@@ -9,6 +9,13 @@ package lab_05;
  * @author 6298805
  */
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Task1 extends Application {
@@ -18,6 +25,23 @@ public class Task1 extends Application {
 
     @Override
     public void start(Stage stage) {
+        Label styleLabel = new Label("Select bag style:");
         
+        ListView<String> styleList = new ListView<>();
+        styleList.setMaxHeight(150);
+        styleList.getItems().addAll(
+                "Full decorative", "Beaded", "Pirate design", "Fringed", "Leather", "Plain");
+        
+        HBox selectionHBox = new HBox();
+        selectionHBox.setPadding(new Insets(25));
+        selectionHBox.setSpacing(25);
+        selectionHBox.setAlignment(Pos.CENTER);
+        selectionHBox.getChildren().addAll(styleLabel, styleList);
+        
+        Scene scene = new Scene(selectionHBox);
+        
+        stage.setScene(scene);
+        stage.setTitle("Bag Order Form");
+        stage.show();
     }
 }
